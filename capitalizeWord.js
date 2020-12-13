2df46d097edf6c275c30c1c29af94e4c9be6a931
Julie <julieseo.dev@gmail.com>
2020-12-13 15:46:47 -0800

    create capitalzeWord

---
+++ capitalizeWord.js
@@ -0,0 +1,10 @@
+// function capitalizeWord(word) {
+//     return word.toUpperCase();
+// }
+
+const capitalizeWord = (word) => {
+    return word.toUpperCase();
+}
+
+console.log(capitalizeWord("julie")) // "JULIE"
+console.log(capitalizeWord("peter")) // "PETER"
--- notes.js
+++ notes.js
@@ -29,3 +29,75 @@ function functionName() {
 const functionName = () => {
 
 }
+
+
+
+// variable
+// you can declare a variable
+const name;
+let name;
+
+// you can define a variable
+name = 'Julie';
+
+const name = 'Julie'; // once assigned, cannot change the value
+let name = 'Julie'; // you can change the value
+
+
+let age;
+age = 35;
+age = 36;
+age = 37;
+console.log(age); // 37
+
+const age2 = 35;
+
+
+// string
+"bootcamp".length // 8
+"bootcamp".toUpperCase() // 'BOOTCAMP'
+"BOOTcaMP".toLowerCase() // 'bootcamp'
+"bootcamp"[3] // 't'
+"bootcamp".indexOf("t") // 3
+
+let word = "bootcamp";
+word.toUpperCase() // 'BOOTCAMP'
+word[3] // 't'
+
+
+
+
+
+// boolean (true / false)
+
+// and && (both need to be true, in order to be true)
+true && true // true
+true && false // false
+false && true // false
+false && false // false
+
+// or || (at least one is true, then true)
+true || true // true
+true || false // true
+false || true // true
+false || false // false
+
+
+
+// conditional
+if (condition) {
+    //do some code...
+} else {
+    //do some other code...
+}
+
+
+if (condition1) {
+    //do something...
+} else if (condition2) {
+    //do something else...
+} else if (condition3) {
+    //do something else...
+} else {
+    //do something if none of the conditions above are true
+}

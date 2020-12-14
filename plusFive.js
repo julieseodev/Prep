91edb673f41dbb372004276bb0024dd7e3ba00c4
Julie <julieseo.dev@gmail.com>
2020-12-13 16:04:29 -0800

    create plusFive

---
+++ plusFive.js
@@ -0,0 +1,10 @@
+function plusFive(num) {
+    return num + 5;
+}
+
+console.log(plusFive(8)); // 13
+console.log(plusFive(13)); // 18
+console.log(plusFive(0)); // 5
+console.log(plusFive(-3)); // 2
+console.log(plusFive(100)); // 105
+

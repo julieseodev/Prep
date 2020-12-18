c976e944cd6cf734635833c0ddf84b5572f6b1e0
Julie <julieseo.dev@gmail.com>
2020-12-17 22:17:55 -0800

    update notes2

---
+++ Object/getFullName.js
@@ -0,0 +1,7 @@
+// Write a function 'getFullName(person)' that takes an object
+// returns a string containing their full name
+
+// ex)
+// let p1 = { firstName: 'John', lastName: 'Doe' };
+// getFullName(p1); // 'John Doe'
+
---
+++ Object/notes2.js
@@ -0,0 +1,78 @@
+let bootcamp = {
+    // key: value
+    name: 'Codecademy',
+  
+    founders: ['Hank', 'Nathan'],
+  
+    locations: ['SF', 'NY'],
+  
+    students: ['Tommy', 'Fred', 'Meagan', 'Alex'],
+  
+    population: 1000,
+
+    enroll: function (studentName) {
+        this.students.push(studentName);
+        this.population += 1;
+        return this.population;
+    }
+};
+
+// console.log(bootcamp.students); // ['Tommy', 'Fred', 'Meagan', 'Alex']
+// console.log(bootcamp.enroll('Donald')); 
+// console.log(bootcamp.students); // ['Tommy', 'Fred', 'Meagan', 'Alex', 'Donald']
+
+
+// Object methods
+let keys = Object.keys(bootcamp); // [ 'name', 'founders', 'locations', 'students', 'population', 'enroll' ]
+let values = Object.values(bootcamp); // [ 'Codecademy', ['Hank', 'Nathan'], ['SF', 'NY'], ['Tommy', 'Fred', 'Meagan', 'Alex'], 1000, function ]
+
+let obj = {
+    name: 'Sam',
+    age: 25,
+    gender: 'male'
+}
+
+for (let key in obj) {
+    console.log(key); // 'name', 'age', 'gender'
+}
+
+for (let key in obj) {
+    console.log(obj[key]); // 'Sam', 25, 'male'
+}
+
+Object.keys(obj).forEach((key) => {
+
+})
+
+for (let key in obj) {
+    console.log(key + ": " + obj[key]);
+    // name: Sam
+    // age: 25
+    // gender: male    
+}
+
+for (let key in obj) {
+    console.log(key.toUpperCase() + ": " + obj[key]);
+    // NAME: Sam
+    // AGE: 25
+    // GENDER: male 
+}
+
+for (let key in obj) {
+    console.log(key[0].toUpperCase() + key.slice(1).toLowerCase() + ": " + obj[key]);
+    // console.log(key.slice(0,1).toUpperCase() + key.slice(1).toLowerCase() + ": " + obj[key]);
+
+    // Name: Sam
+    // Age: 25
+    // Gender: male 
+}
+
+
+// > str = "JULIE"
+// 'JULIE'
+// > str.slice(0)
+// 'JULIE'
+// > str.slice(1)
+// 'ULIE'
+// > str.slice(1,3)
+// 'UL'

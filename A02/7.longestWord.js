28bcbb935ce580ff20d49b21bee4e25bee5988fb
Julie <julieseo.dev@gmail.com>
2020-12-19 23:26:29 -0800

    create longestWord

--- A02/7.longestWord.js
+++ A02/7.longestWord.js
@@ -4,8 +4,25 @@ If there are ties, the function should return the later word.
 
 Examples:
 
-longestWord('app academy is cool'); // => 'academy'
+longestWord('coding school is cool'); // => 'school'
 longestWord('hate having hungry hippos'); // => 'hippos'
 longestWord('bootcamp'); // => 'bootcamp'
 longestWord(''); // => ''
 *******************************************************************************/
+
+function longestWord(sentence) { // 'coding school is cool'
+    let words = sentence.split(' '); // ['coding', 'school', 'is', 'cool']
+    let longest = "";              
+    for (let i = 0; i < words.length; i++) {  // i = 0,1,2,3
+        let word = words[i]; // 'coding' 'school'
+        if (word.length >= longest.length) { // 6 >= 6
+            longest = word; // longest = 'school'
+        }
+    }   
+    return longest;
+}
+
+console.log(longestWord('coding school is cool')); // => 'school'
+console.log(longestWord('hate having hungry hippos')); // => 'hippos'
+console.log(longestWord('bootcamp')); // => 'bootcamp'
+console.log(longestWord('')); // ''

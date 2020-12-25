c8608b8f183d6dbd0e41cbfaee5977fc3b1b7c0f
Julie Seo <julieseo.dev@gmail.com>
2020-12-24 23:58:17 -0800

    update hasSymmetry

--- Array/2.hasSymmetry.js
+++ Array/2.hasSymmetry.js
@@ -10,11 +10,27 @@ In other words, `[1, 2, 3] === [1, 2, 3]` evaluates to false.
 Examples:
 
 hasSymmetry([1, 2, 3, 3, 2, 1]) => true
+hasSymmetry([1, 2, 3, 2, 1]) => true
 hasSymmetry([1, 2, 3, 3, 4, 1]) => false
 hasSymmetry(['cat', 'dog', 'bird', 'dog', 'cat']) => true
 hasSymmetry(['cat', 'dog', 'bird', 'bird', 'cat']) => false
 *******************************************************************************/
 
 function hasSymmetry(array) {
-    // your code here...
-  }
+    for (let i = 0; i < array.length / 2; i++) {
+        let j = array.length - 1 - i;
+        if (array[i] !== array[j]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(hasSymmetry([1, 2, 3, 3, 2, 1])); // => true
+console.log(hasSymmetry([1, 2, 3, 2, 1])); // => true
+console.log(hasSymmetry([1, 2, 3, 3, 4, 1])); // => false
+console.log(hasSymmetry(['cat', 'dog', 'bird', 'dog', 'cat'])) // => true
+console.log(hasSymmetry(['cat', 'dog', 'bird', 'bird', 'cat'])) // => false
+// Math.ceil(2.5) // 3
+// Math.floor(2.5) // 2

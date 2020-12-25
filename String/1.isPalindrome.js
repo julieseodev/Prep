6ae9fd63353596291b65a07c7c00f10ab3909966
Julie Seo <julieseo.dev@gmail.com>
2020-12-25 01:08:23 -0800

    update isPalindrome

--- String/1.isPalindrome.js
+++ String/1.isPalindrome.js
@@ -12,5 +12,20 @@ isPalindrome('hello world'); // => false
 *******************************************************************************/
 
 function isPalindrome(sentence) {
-    
+  let arr = sentence.split(' ');
+  let newSentence = arr.join('');
+
+  // console.log(newSentence);
+  
+  for (let i = 0; i < newSentence.length / 2; i++) {
+    let j = newSentence.length - 1 - i;
+    if (newSentence[i] !== newSentence[j]) {
+      return false;
+    }
   }
+  return true;
+}
+console.log(isPalindrome('rats live on no evil star')); // => true
+console.log(isPalindrome('stella won no wallets')); // => true
+console.log(isPalindrome('racecar')); // => true
+console.log(isPalindrome('hello world')); // => false

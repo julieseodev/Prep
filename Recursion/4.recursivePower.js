4ab498242335ae52f9fb804442785fc755b78d40
Julie Seo <julieseo.dev@gmail.com>
2020-12-27 16:30:32 -0800

    create recursivePower

--- Recursion/4.recursivePower.js
+++ Recursion/4.recursivePower.js
@@ -10,3 +10,20 @@
 // power(9, 3); => 729
 // power(11, 0); => 1
 // power(11, 1); => 11
+
+function power(base, exp) {
+    if (exp <= 0) {     // exp === o
+        return 1;
+    } else {
+        let result = base * power(base, exp - 1);
+        return result;
+    }
+}
+
+
+console.log(power(2, 5)); // => 32
+console.log(power(2, 10)); // => 1024
+console.log(power(9, 2)); // => 81
+console.log(power(9, 3)); // => 729
+console.log(power(11, 0)); // => 1
+console.log(power(11, 1)); // => 11

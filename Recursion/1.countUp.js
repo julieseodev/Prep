9fc0b0b969b3d24853d737368bb5316702b62733
Julie Seo <julieseo.dev@gmail.com>
2020-12-27 16:01:43 -0800

    create countUp

---
+++ Recursion/1.countUp.js
@@ -0,0 +1,43 @@
+/*************************************************************************************
+Write a recursive function countUp(num) that takes in a number and prints all whole 
+numbers from num to 10. It should print 'done!' upon reaching 10 or greater. Do this 
+recursively, without loops. Reference the countdown example from the recursion notes.
+Hint: use if (num >= 10)... as the base case
+
+
+Example:
+countUp(3); prints:
+3
+4
+5
+6
+7
+8
+9
+done!
+countUp(20);
+done!
+*************************************************************************************/
+
+function countUp(num) {
+    if (num >= 10) {
+        console.log("done!");
+        return;
+    } else {
+        console.log(num); 
+        countUp(num + 1);
+    }
+}
+
+countUp(3); 
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// done!
+
+countUp(20);
+// done!
---
+++ Recursion/3.recursiveFactorial.js
@@ -0,0 +1,12 @@
+/******************************************************************************************
+Write a recursive function factorial(n), that returns the factorial of the number n. 
+For example, the factorial of 4 is 4 * 3 * 2 * 1 = 24.
+Hint: use if (n === 1)... as the base case.
+
+
+Examples:
+factorial(1); => 1
+factorial(4); => 24
+factorial(5); => 120
+factorial(10); => 3628800
+********************************************************************************************/

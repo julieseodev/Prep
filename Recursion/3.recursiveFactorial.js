f10fa5573d6fe3fd95ee986a4a712e47c42a8a53
Julie Seo <julieseo.dev@gmail.com>
2020-12-27 16:19:10 -0800

    create revursiveFactorial

--- Recursion/3.recursiveFactorial.js
+++ Recursion/3.recursiveFactorial.js
@@ -10,3 +10,18 @@ factorial(4); => 24
 factorial(5); => 120
 factorial(10); => 3628800
 ********************************************************************************************/
+
+function factorial(n) {
+    if (n === 1) {
+        return 1;
+    } else {
+        let result = n * factorial(n - 1);
+        return result;
+    }
+    
+}
+
+console.log(factorial(1)); // => 1
+console.log(factorial(4)); // => 24
+console.log(factorial(5)); // => 120
+console.log(factorial(10)); // => 3628800

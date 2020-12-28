b8cda7f903d82300e0582a30851b7d843298e343
Julie Seo <julieseo.dev@gmail.com>
2020-12-27 17:21:09 -0800

    create recursiveFib

--- Recursion/7.recursiveFib.js
+++ Recursion/7.recursiveFib.js
@@ -10,3 +10,20 @@
 // fib(5) => 5
 // fib(6) => 8
 // fib(7) => 13
+
+function fib(n) {
+    if (n === 1 || n === 2) {
+        return 1;
+    } else {
+        let result = fib(n - 1) + fib(n - 2);
+        return result;
+    }
+}
+
+console.log(fib(1)); // => 1
+console.log(fib(2)); // => 1
+console.log(fib(3)); // => 2
+console.log(fib(4)); // => 3
+console.log(fib(5)); // => 5
+console.log(fib(6)); // => 8
+console.log(fib(7)); // => 13

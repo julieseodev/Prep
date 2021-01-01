bb626fedf470a6439d715d5da503cd9677e59066
Julie Seo <julieseo.dev@gmail.com>
2020-12-31 21:48:49 -0800

    create toFeet

--- A06/3.toFeet.js
+++ A06/3.toFeet.js
@@ -8,3 +8,29 @@
 
 // var arr2 = '2 yards', '3 feet', '10 yards';
 // toFeet(arr2); =>  '6 feet', '3 feet', '30 feet' 
+
+
+function toFeet(distances) {
+    let newArr = [];
+    for (let i = 0; i < distances.length; i++) {
+        // let distance = distances[i].split(' '); // arr
+        let distance = distances[i];
+        let distanceArr = distance.split(" ");
+        if (distanceArr[1] === 'yards') {
+            newArr.push(3 * distanceArr[0] + ' feet');
+        } else {
+            newArr.push(distances[i]);
+        }    
+    }
+    return newArr;
+}
+
+var arr1 = ['10 feet', '3 yards', '5 yards', '3 feet'];
+console.log(toFeet(arr1)); // => ['10 feet', '9 feet', '15 feet', '3 feet'] 
+
+
+var arr2 = ['2 yards', '3 feet', '10 yards'];
+console.log(toFeet(arr2)); // => ['6 feet', '3 feet', '30 feet'] 
+
+
+

73c238f19b29bf3218cd8ad8b5941774c5749b4e
Julie Seo <julieseo.dev@gmail.com>
2021-01-02 19:27:46 -0800

    create shiftChars

--- A06/18.shiftChars.js
+++ A06/18.shiftChars.js
@@ -13,3 +13,20 @@
 // shiftChars('apple', 2); => 'crrng'
 // shiftChars('bootcamp', 3); => 'errwfdps'
 // shiftChars('zebra', 5); => 'ejgwf'
+
+function shiftChars(word, num) {
+    let newStr = "";
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    for (let i = 0; i < word.length; i++) {
+        let char = word[i];
+        let index = alphabet.indexOf(char);
+        let newIndex = (index + num) % 26; 
+        newStr += alphabet[newIndex];
+    }
+    return newStr;
+}
+
+console.log(shiftChars('able', 1)); // => 'bcmf'
+console.log(shiftChars('apple', 2)); // => 'crrng'
+console.log(shiftChars('bootcamp', 3)); // => 'errwfdps'
+console.log(shiftChars('zebra', 5)); // => 'ejgwf'
